b73f5dd43170a1636594a0ac6fd21beccc9c3cf8
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-05-03 18:22:58 +0800

    Update inspection.js

--- api/inspection.js
+++ api/inspection.js
@@ -23,7 +23,8 @@ export default async function handler(req, res) {
     const gasRes = await fetch(GAS_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ action, ...payload })
+      body: JSON.stringify({ action, token, ...payload })
+
     });
 
     const result = await gasRes.json();
