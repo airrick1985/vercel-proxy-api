0083a307697974d149067cfe751a6cc296246bab
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-07-22 15:56:52 +0800

    Update sales.js

--- api/sales.js
+++ api/sales.js
@@ -43,7 +43,8 @@ export default async function handler(req, res) {
                           'get_parking_lot_details',
                           'update_parking_lot_details',
                           'download_sheets_as_excel',
-                          'upload_excel_to_overwrite'
+                          'upload_excel_to_overwrite',
+                           'upload_excel_for_overwrite'
                          ];
     if (!action || !allowActions.includes(action)) {
       return res.status(400).json({ status: 'error', message: `Action "${action}" is not supported by this endpoint.` });
